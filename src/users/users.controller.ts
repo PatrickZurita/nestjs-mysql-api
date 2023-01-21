07425e149dae730fd3557de714f084718ff94977
pTrickDev <viericartman@gmail.com>
2023-01-21 11:14:50 -0500

    User Crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Get, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param, ParseIntPipe, Delete, Patch } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -19,7 +20,16 @@ export class UsersController {
 
   @Get(':id')
   getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
-    console.log(id)
     return this.usersService.getUser(id)
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.deleteUser(id)
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id', ParseIntPipe) id: number, @Body() user: UpdateUserDto) {
+    return this.usersService.updateUser(id, user)
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -23,5 +24,13 @@ export class UsersService {
         id
       }
     })
+  }
+
+  deleteUser(id: number) {
+    return this.userRepository.delete({ id })
+  }
+
+  updateUser(id: number, user: UpdateUserDto) {
+    return this.userRepository.update({ id }, user)
   }
 }
